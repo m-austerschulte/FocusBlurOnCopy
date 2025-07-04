e4bbadb78cfe3031521071a7bb3d268bc2e7ce32
Marius Austerschulte <m.austerschulte@conterra.de>
2025-07-04 13:45:15 +0200

    Format file

--- src/main/js/bundles/focus-blur-on-copy/CopyButton.ts.vue
+++ src/main/js/bundles/focus-blur-on-copy/CopyButton.ts.vue
@@ -2,14 +2,14 @@
     <v-btn @click="copy('Hi')">Copy to clipboard</v-btn>
 </template>
 <script lang="ts">
-    import Vue from "apprt-vue/Vue";
-    import {copyText} from "apprt-vue/directives/clipboard";
+import Vue from "apprt-vue/Vue";
+import {copyText} from "apprt-vue/directives/clipboard";
 
-    export default Vue.extend({
-        methods: {
-            copy(text: string): void {
-                copyText(text, this.$el);
-            }
+export default Vue.extend({
+    methods: {
+        copy(text: string): void {
+            copyText(text, this.$el);
         }
-    });
+    }
+});
 </script>
